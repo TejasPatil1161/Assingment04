4c077557ef00e978e16d30f1a3838dfa05f4bccd
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-20 18:21:52 +0530

    Create first 10 odd natural numbers in reverse order05.c

---
+++ first 10 odd natural numbers in reverse order05.c
@@ -0,0 +1,11 @@
+//5. Write a program to print the first 10 odd natural numbers in reverse order.
+#include<stdio.h>
+ int main()
+ {
+ 	int i;
+ 	printf("first 10 odd natural numbers in reverse order\n");
+ 	for(i=19; i>0;i--)
+ 	{
+ 		printf("%d\n",i);
+	 }
+ }
