41f58383febe852ca135bc314f056dfd0f751d55
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-20 18:17:28 +0530

    Create  first 10 natural numbers in reverse order04.c

---
+++ first 10 natural numbers in reverse order04.c
@@ -0,0 +1,11 @@
+//3. Write a program to print the first 10 natural numbers in reverse order
+#include<stdio.h>
+ int main()
+ {
+ 	int i;
+ 	printf("first 10 natural numbers revers order\n");
+ 	for(i=10;i>0;i--)
+ 	{
+ 		printf("%d\n",i);
+	 }
+ }
