d4979b788fa77de9cb7ebd775af2f9e70b08cce6
TejasPatil1161 <95114366+TejasPatil1161@users.noreply.github.com>
2022-09-20 18:20:53 +0530

    Create first 10 odd natural numbers04.c

---
+++ first 10 odd natural numbers04.c
@@ -0,0 +1,12 @@
+//4. Write a program to print the first 10 odd natural numbers
+#include<stdio.h>
+ int main()
+ {
+ 	int i;
+ 	printf("first 10 odd natural numbers\n");
+ 	for(i=1;i<=19;i=i+2)
+ 		{
+ 		  printf("\n %d",i);
+ 	    }
+	 
+ }
